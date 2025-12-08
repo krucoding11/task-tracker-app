7f120c650e2cc7afe172cb5f3478b46051ecc53f
rutu-edelta <rutu.patel@edeltaes.com>
2025-12-08 13:22:48 +0530

    add response for reference and 3 new api

--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { getTasksByEmployeeId } = require("../controllers/taskController.js");
+const {
+  getTasksByEmployeeId,
+  getTasksByProject,
+  logTime,
+  getTimeEntriesForTask,
+} = require("../controllers/taskController.js");
 
 const { protect, authorize } = require("../middleware/authMiddleware");
 
@@ -9,6 +14,20 @@ router.get(
   protect,
   authorize("tasks:manage", "tasks:read"),
   getTasksByEmployeeId,
+);
+router.get(
+  "/project/:projectId",
+  protect,
+  authorize("tasks:read"),
+  getTasksByProject,
+);
+router.post("/log-time", protect, authorize("tasks:log:time"), logTime);
+
+router.get(
+  "/:taskId/time-entries",
+  protect,
+  authorize("tasks:manage"),
+  getTimeEntriesForTask,
 );
 
 module.exports = router;
